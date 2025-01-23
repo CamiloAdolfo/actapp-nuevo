0b75bf81804c12e29fa7f2d3e35b33ec0543bd9e
Camilo <pattern2006@gmail.com>
2025-01-23 15:43:37 -0500

    Corregir configuración de Next.js y importación de ActaForm

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ["sqlite3"],
   experimental: {
-    
+    serverComponentsExternalPackages: ["sqlite3"],
   },
 }
 
